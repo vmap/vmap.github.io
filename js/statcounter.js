1f7093896044164f95be52c5f81ea97a25f8f855
dr3wh0p5@gmail.com <drewhopskotch@yahoo.com>
2013-11-02 20:06:00 -0500

    install statcounter

--- js/statcounter.js
+++ js/statcounter.js
@@ -1,3 +1,4 @@
+//<![CDATA[
 var sc_project=9205437; 
 var sc_invisible=1; 
 var sc_security="5fae848d"; 
@@ -5,9 +6,5 @@ var scJsHost = (("https:" == document.location.protocol) ?
 "https://secure." : "http://www.");
 document.write("<sc"+"ript type='text/javascript' src='" +
 scJsHost+
-"statcounter.com/counter/counter.js'></"+"script>");
-<noscript><div class="statcounter"><a title="site stats"
-href="http://statcounter.com/free-web-stats/"
-target="_blank"><img class="statcounter"
-src="http://c.statcounter.com/9205437/0/5fae848d/1/"
-alt="site stats"></a></div></noscript>
+"statcounter.com/counter/counter_xhtml.js'></"+"script>");
+//]]>
